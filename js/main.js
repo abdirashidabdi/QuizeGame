67cc033bd364249c3625c144f44de28da5ca08a9
abdirashidabdi <abdirashidmahad52@gmail.com>
2020-03-04 20:10:02 -0500

    join page js updated

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
-let buttonJoin = document.querySelector(".btn");
-let gamePin = 469884;
-let gameInput = document.querySelector('#input');
+// joinGame class
+class joinGame {
+  // Generate random game pin
+  static generatePin() {
+    const randomPin = Math.floor(Math.random() * 1000000 + 1);
+    return randomPin;
+  }
 
-buttonJoin.addEventListener('click', joinGame);
+  // store game pin in cookies
+  static storePin() {
+    let gamePin = joinGame.generatePin();
+    return (document.cookie = gamePin);
+  }
 
-function joinGame(){
-    if(gameInput.value == gamePin){
-        if ((window.event.keyCode = 13 || buttonJoin.event == 'click')){
-            window.location.assign("http://127.0.0.1:5501/form.html");
-           alert('joined The game')
-        }
+  // retrieve game pin from cookies
+  static retrievePin() {
+    let PIN = document.cookie;
+    document.getElementById("pin").innerText = PIN;
+    return PIN;
+  }
+  static getInputValue(gameInput) {
+    let inputValue = gameInput;
+    joinGame.join(inputValue);
+    return inputValue;
+  }
+
+  //Join method
+  static join(inputValue) {
+    let joinPin = r;
+    let input = inputValue;
+    console.log(input);
+    if (input === joinPin) {
+      console.log(input);
+      window.location.href = "http://www.w3schools.com";
+    } else if (input == "") {
+      alert("game pin can not be blank");
+    } else if (input !== joinPin) {
+      alert("invalid game pin");
     }
-    else if(gameInput.value !== gamePin){
-       alert("Game Pin is not Valid")
-    }
-    else{
-       alert("Game Pin is required")
-    }
+  }
+
+  // clearField
 }
+
+// addEventLister to the form
+document
+  .querySelector(".join-form")
+  .addEventListener("submit", joinGame.getInputValue);
+// g = joinGame.generatePin();
+// s = joinGame.storePin();
+// r = joinGame.retrievePin();
+// get = joinGame.getInputValue();
+// j = joinGame.join();
